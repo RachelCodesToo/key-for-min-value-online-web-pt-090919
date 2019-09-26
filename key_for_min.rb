1c3b6c7266a39125101d6405860ce12e4abef3e9
Anne Fausto <a.r.fausto@gmail.com>
2019-09-26 15:46:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, value|
-    return name.max.reverse
+  name_hash.collect do |keys, value|
+    return keys.max.reverse
   end
 
 end
