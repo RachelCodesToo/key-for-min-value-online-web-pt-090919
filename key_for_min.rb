acc8660948a7f72296dcc104d9e486ba7afe68bc
Anne Fausto <a.r.fausto@gmail.com>
2019-09-26 16:27:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,12 @@
 
 def key_for_min_value(name_hash)
   current_min_value = ""
+  current_key = ""
   name_hash.each do |key, value|
-    if value < new_val
-      
-  def 
+    if value < current_min_value
+      current_min_value = value
+      current_key = key
+    end
+  end
+  current_key
+end
